9434383c7ecdce61e04ee2ee9e35a24416b7c0d6
toledanogus <toledanogu@gmail.com>
2021-10-18 21:35:38 -0500

    Respuestas en js

--- js/esp.js
+++ js/esp.js
@@ -1,8 +1,17 @@
-
-const fun = () => {
-    const r1 = document.querySelector('input[name="1"]:checked').value;
-    console.log(r1);    
+let puntaje;
+const fun = (e) => {
+    let respuestas = [];
+    for (let i = 1; i < 3; i++) {
+        respuestas.push(document.querySelector(`input[name="${i}"]:checked`).value);
+    }
+    console.log(respuestas);
+    e.preventDefault();
+    const counts = {};
+    respuestas.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+    puntaje=counts.rc;
+    console.log(puntaje);
 }
+
 
 const boton = document.querySelector('.boton1');
 boton.addEventListener('click', fun);
