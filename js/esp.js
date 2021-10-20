b8438b405821c5fde031f5cceac2ab5671fe1e17
toledanogus <toledanogu@gmail.com>
2021-10-20 18:02:37 -0500

    Con antitrampa y 11 preguntas

--- js/esp.js
+++ js/esp.js
@@ -7,7 +7,7 @@ console.log(nombre);
 const fun = (e) => {
     let respuestas = [];
     let respuestasCompletas = [];
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i < 12; i++) {
         respuestas.push(document.querySelector(`input[name="${i}"]:checked`).value);
         respuestasCompletas.push(document.querySelector(`input[name="${i}"]:checked`).nextElementSibling.innerText);
     }
@@ -18,7 +18,7 @@ const fun = (e) => {
     respuestas.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
     puntaje=counts.rc;
     console.log(puntaje);
-    let promedioesp = (puntaje*10)/3
+    let promedioesp = (puntaje*10)/11
     console.log(promedioesp);
     if (puntaje==undefined) {
         alert('Ninguna de tus respuestas es correcta ¿Deseas volver a intentarlo?');
@@ -27,6 +27,14 @@ const fun = (e) => {
     datosJson["xxx"]= respuestasCompletas[0];
     datosJson["xxx1"]= respuestasCompletas[1];
     datosJson["xxx2"]= respuestasCompletas[2];
+    datosJson["xxx3"]= respuestasCompletas[3];
+    datosJson["xxx4"]= respuestasCompletas[4];
+    datosJson["xxx5"]= respuestasCompletas[5];
+    datosJson["xxx6"]= respuestasCompletas[6];
+    datosJson["xxx7"]= respuestasCompletas[7];
+    datosJson["xxx8"]= respuestasCompletas[8];
+    datosJson["xxx9"]= respuestasCompletas[9];
+    datosJson["xxx10"]= respuestasCompletas[10];
     datosJson["nom"]= nombre;
     datosJson["ape"]= apellido;
     datosJson["espP"]= promedioesp;
--- php/esp.php
+++ php/esp.php
@@ -7,6 +7,16 @@ $jsonObj = json_decode($str_json);
 
 //Con mysqli_query se hace una petición a la base de datos, ya sea para llamar datos, modificar o insertar datos
 //mysqli_query($conn, "INSERT INTO examen1 (espP, uno, dos, tres) VALUES ('".$jsonObj->espP."','".$jsonObj->xxx."','".$jsonObj->xxx1."', '".$jsonObj->xxx2."')");
-mysqli_query($conn, "UPDATE examen1 SET espP='".$jsonObj->espP."', uno='".$jsonObj->xxx."', dos='".$jsonObj->xxx1."', tres='".$jsonObj->xxx2."' WHERE nombre= '".$jsonObj->nom."' AND apellidos= '".$jsonObj->ape."'");
+$verificar = mysqli_query($conn, "SELECT * FROM examen1 WHERE nombre= '".$jsonObj->nom."' AND apellidos= '".$jsonObj->ape."'");
+$row = mysqli_fetch_array($verificar);
+$valor = $row['antitrampa'];
+
+if ($valor >= 1) {
+  echo "Error";
+  }
+else {
+    
+    mysqli_query($conn, "UPDATE examen1 SET antitrampa=1, espP='".$jsonObj->espP."', uno='".$jsonObj->xxx."', dos='".$jsonObj->xxx1."', tres='".$jsonObj->xxx2."', cuatro='".$jsonObj->xxx3."', cinco='".$jsonObj->xxx4."', seis='".$jsonObj->xxx5."', siete='".$jsonObj->xxx6."', ocho='".$jsonObj->xxx7."', nueve='".$jsonObj->xxx8."', diez='".$jsonObj->xxx9."', once='".$jsonObj->xxx10."' WHERE nombre= '".$jsonObj->nom."' AND apellidos= '".$jsonObj->ape."'");
+}
 
 ?>
