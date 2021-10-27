879f8b653baaa9d7d9fc9a553ae149249098fbb3
toledanogus <toledanogu@gmail.com>
2021-10-26 20:58:22 -0500

    Version 1.3 localhost

--- php/his.php
+++ php/his.php
@@ -1,6 +1,6 @@
 <?php
 //Conectar a la base de datos
-$conn = mysqli_connect('localhost', 'toledano_1', 'sfreud1978', 'toledano_pedrueza') or die(mysqli_error($conn));
+$conn = mysqli_connect('localhost', 'gustavich', 'toledano', 'toledanok') or die(mysqli_error($conn));
 
 $str_json = file_get_contents("php://input");
 $jsonObj = json_decode($str_json);
